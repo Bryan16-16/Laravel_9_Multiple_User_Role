2d7e18a70ccaad95093d86e0babeecb5b6986cc7
Bryan Nathaniel <bryan2ndacc@gmail.com>
2022-09-04 15:20:47 +0800

    footer added in hero page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,15 +40,15 @@
                         <a style="font-weight: bold; color:#607ebd" class="nav-link active" aria-current="page" href="#">Home</a>
                       </li>
                       <li class="nav-item">
-                        <a style="font-weight: bold; color:#607ebd;" class="nav-link" href="#">Services</a>
+                        <a style="font-weight: bold; color:#607ebd;" class="nav-link" href="#service">Services</a>
                       </li>
                       
                       <li class="nav-item">
-                        <a style="font-weight: bold; color:#607ebd;" class="nav-link" href="#">Location</a>
+                        <a style="font-weight: bold; color:#607ebd;" class="nav-link" href="#location">Location</a>
                       </li>
 
                       <li class="nav-item">
-                        <a style="font-weight: bold; color:#607ebd;" class="nav-link" href="#">About us</a>
+                        <a style="font-weight: bold; color:#607ebd;" class="nav-link" href="#about">About us</a>
                       </li>
                       
                       <li class="nav-item">
@@ -59,10 +59,8 @@
                     
                   </div>
    
+{{-- old position of button hamburger menu below --}}
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -106,7 +104,14 @@
                         @endguest
                     </ul>
                 </div>
+
+
             </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
         </nav>
 
    <div class="container-md">
@@ -125,24 +130,45 @@
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam dolor ex aliquid obcaecati ea illum sequi soluta quos incidunt porro, ipsam repellendus adipisci ipsa voluptate similique laboriosam fugit dolorum labore!
 </p>
 
+<br><br>
+<br><br>
+<br><br>
+  <h3 id="service"><strong>Services</strong></h3>
 
-<br>
-<br><br><br><br>
 
 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur laudantium quisquam culpa aliquid odio nobis totam veniam distinctio odit fuga quis atque quas perferendis assumenda, repudiandae velit cum, animi fugit.
 <br><br>
-<h3>location</h3>
+<h3 id="location"><strong>location</strong></h3>
    <div class="ratio"  style="--bs-aspect-ratio: 30%;">
     <iframe src="https://maps.google.com/maps?q=Grateful%20Beginnings%20Medical,%20Pediatric,%20Dental%20and%20Lying%20in%20Clinic,%20Calamba,%204027%20Laguna&t=&z=13&ie=UTF8&iwloc=&output=embed" title="Maps" ></iframe>
   </div>
 
+
+  <br><br>
+  <h3 id="about"><strong>About us</strong></h3>
+
   <br><br><br>
+
+  
    </div>
 
     
     
+   <footer  class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div  class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span style="color:white;" class="mb-3 mb-md-0">&copy; 2022 Company, Inc</span>
+    </div>
 
-
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      
+      <li class="ms-3"><a class="text-muted" href="#"><span style="color:white;" data-feather="facebook" class="align-text-bottom"></span></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><span style="color:white;" data-feather="instagram" class="align-text-bottom"></span></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+    </ul>
+  </footer>
 </body>
 <script type="text/javascript" src="{{ mix('resources/js/app.js') }}"></script>
 <script>
