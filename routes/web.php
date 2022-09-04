bb98e16d7a75854d4b72cb2975157cd0189b298a
Bryan Nathaniel <bryan2ndacc@gmail.com>
2022-09-04 13:07:50 +0800

    register error fix

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::get('/', function () {
 });
 
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Auth::routes();
   
 /*------------------------------------------
